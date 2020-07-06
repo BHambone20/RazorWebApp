f0ab523352315704ea71d5c639c4dcbc77d8dc06
Brian Hammond <Hammond.Brian@gmail.com>
2020-07-05 21:38:37 -0400

    Add project files.

---
+++ Pages/EmailRegistration.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
+
+namespace RazorWebApp.Pages
+{
+    public class EmailRegistrationModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+        public async Task OnPostAsync()
+        {
+            try
+            {
+                var emailAddress = Request.Form["emailaddress"];
+                using (var smtp = new SmtpClient())
+                {
+                    smtp.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+                    smtp.PickupDirectoryLocation = @"c:\sql";
+                    var message = new MailMessage
+                    {
+                        Body = "Thank you for registering your email information.",
+                        Subject = "Registration Complete",
+                        From = new MailAddress("BrianHammond@usf.edu")
+                    };
+                    message.To.Add(emailAddress);
+                    await smtp.SendMailAsync(message);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                //ex.ToString;
+            }
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace RazorWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public string Message { get; set; }
+
+        public string Message1 { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Message = "Hello";
+            Message1 = "Wow it worked!";
+        }
+    }
+}
